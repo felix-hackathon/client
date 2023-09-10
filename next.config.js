32341841048fe58b2075d26e35e4a361a7be952a
nhatminh0509 <leminhnhat1133@gmail.com>
2023-09-10 21:57:17 +0700

    chore: sw

--- next.config.js
+++ next.config.js
@@ -24,38 +24,34 @@ if (process.env.NODE_ENV === 'development') {
   })
 }
 if (process.env.NODE_ENV === 'production') {
-  const withPWANext = require('next-pwa')
-
-  const withPWA = withPWANext({
-    dest: 'public',
-    buildExcludes: ['app-build-manifest.json'],
-  })
-
-  const generateAppDirEntry = (entry) => {
-    const packagePath = require.resolve('next-pwa')
-    const packageDirectory = path.dirname(packagePath)
-    const registerJs = path.join(packageDirectory, 'register.js')
-
-    return entry().then((entries) => {
-      if (entries['main-app'] && !entries['main-app'].includes(registerJs)) {
-        if (Array.isArray(entries['main-app'])) {
-          entries['main-app'].unshift(registerJs)
-        } else if (typeof entries['main-app'] === 'string') {
-          entries['main-app'] = [registerJs, entries['main-app']]
-        }
-      }
-      return entries
-    })
-  }
-
-  nextConfig = withPWA({
-    ...nextConfig,
-    webpack: (config) => {
-      const entry = generateAppDirEntry(config.entry)
-      config.entry = () => entry
-      return config
-    },
-  })
+  // const withPWANext = require('next-pwa')
+  // const withPWA = withPWANext({
+  //   dest: 'public',
+  //   buildExcludes: ['app-build-manifest.json'],
+  // })
+  // const generateAppDirEntry = (entry) => {
+  //   const packagePath = require.resolve('next-pwa')
+  //   const packageDirectory = path.dirname(packagePath)
+  //   const registerJs = path.join(packageDirectory, 'register.js')
+  //   return entry().then((entries) => {
+  //     if (entries['main-app'] && !entries['main-app'].includes(registerJs)) {
+  //       if (Array.isArray(entries['main-app'])) {
+  //         entries['main-app'].unshift(registerJs)
+  //       } else if (typeof entries['main-app'] === 'string') {
+  //         entries['main-app'] = [registerJs, entries['main-app']]
+  //       }
+  //     }
+  //     return entries
+  //   })
+  // }
+  // nextConfig = withPWA({
+  //   ...nextConfig,
+  //   webpack: (config) => {
+  //     const entry = generateAppDirEntry(config.entry)
+  //     config.entry = () => entry
+  //     return config
+  //   },
+  // })
 }
 
 module.exports = nextConfig
